cf764e4e5976b9dbc9a4109ba65750bff1458a18
RoccoDr02 <roccodrafz@icloud.com>
2025-09-05 13:42:30 +0200

    made changes added templates, static and json file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # JSON-Datei öffnen und Blogposts laden
+    with open('data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Template rendern und Posts übergeben
+    return render_template('index.html', posts=data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
